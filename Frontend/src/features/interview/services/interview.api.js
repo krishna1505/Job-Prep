06e809ffdc076382d01a211f7ca43c2169c01531
Krishna Mohan Yadav <145401206+krishna1505@users.noreply.github.com>
2026-05-08 15:24:50 +0530

    Update backend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+   baseURL: "https://job-prep-backend-fl4i.onrender.com",
     withCredentials: true,
 })
 
