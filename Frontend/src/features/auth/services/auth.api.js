98e2d5691db439e5f052023007a54b64ad2675ce
krishna@123 <ky2288922@gmail.com>
2026-05-18 14:36:26 +0530

    frontend changes

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 
 const api = axios.create({
-   baseURL: "https://job-prep-back.onrender.com",
-    withCredentials: true
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 })
 
 export async function register({ username, email, password }) {
@@ -47,7 +47,7 @@ export async function logout() {
         return response.data
 
     } catch (err) {
-
+ console.log(err)
     }
 }
 
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://job-prep-back.onrender.com",
-    withCredentials: true,
+   baseURL: import.meta.env.VITE_API_URL,
+   withCredentials: true,
 })
 
 
